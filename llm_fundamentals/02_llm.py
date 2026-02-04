92ce790d402f43370d040de5106cce192fde7ec4
Rodrigo <razevedo.contato@gmail.com>
2026-02-04 08:14:28 -0300

    LLM example 2

---
+++ llm_fundamentals/02_llm.py
@@ -0,0 +1,11 @@
+import openai
+
+client = openai.OpenAI(base_url="https://api.groq.com/openai/v1")
+
+response = client.responses.create(
+    model="llama-3.1-8b-instant",
+    instructions="Responda de forma simples em apenas 1 parágrafo curto.",
+    input="O que é machine learning?",
+)
+
+print(response.output_text)
